df369855b25a51185fd48ca85610a5a28cb8d625
Unleavened Eagle <37382090+UnleavenedEagle@users.noreply.github.com>
2019-03-25 19:56:51 +0100

    Create source.cs

---
+++ source.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SimpleWrapperLauncher
+{
+    class SimpleWrapperLauncher
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string _this = Process.GetCurrentProcess().MainModule.FileName;
+                string _path = Path.GetDirectoryName(_this) + "\\_" + Path.GetFileName(_this);
+                Console.WriteLine(_path);
+                Process.Start(_path, Process.GetCurrentProcess().StartInfo.Arguments);
+                while (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(_path)).Length != 0);
+            }
+            catch (Exception e) { }
+        }
+    }
+}
